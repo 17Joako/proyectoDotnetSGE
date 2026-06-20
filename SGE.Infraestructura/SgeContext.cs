ba586a64822490d9de2aa1048c6dc292e88657e0
TheRecaGamer11 <bautirecalt@hotmail.com>
2026-06-20 00:48:29 -0300

    a

--- SGE.Infraestructura/SgeContext.cs
+++ SGE.Infraestructura/SgeContext.cs
@@ -1,1 +1,14 @@
-    
+using Microsoft.EntityFrameworkCore;
+
+namespace SGE.Infraestructura;
+
+public class SgeContext : DbContext
+{
+    public DbSet<Expediente> Expedientes { get; set; }
+    public DbSet<Tramite> Tramites { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=sge.db");
+    }
+}
